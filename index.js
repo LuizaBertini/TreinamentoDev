60940a1c306797206a4c26f456977f71e4542cbf
Maria Luiza Bertini <maria.bertini@animaeducacao.com.br>
2021-12-16 18:09:20 -0300

    Aula02

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function carregamentoPagina() {
-	alert("Hello world!");
-}
+// function carregamentoPagina() {
+//     alert("Mensagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    //Código executado no carregamento da página
+    // alert("Mensagem apresentada no carregamento do sistema");
+
+    $("#btnSalvar").click(() => {
+        console.log("FUNÇÃO CLICK FUNCIONOU")
+    })
+})
