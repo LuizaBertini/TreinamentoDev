93b835f0f08e4997d6050e7aa16b287fbe3a845e
Maria Luiza Bertini <maria.bertini@animaeducacao.com.br>
2021-12-16 21:06:15 -0300

    Aula03 - Java ScriptII

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-// function carregamentoPagina() {
-//     alert("Mensagem apresentada no carregamento do sistema");
-// }
+function populaTabela() {
+    if (localStorage.getItem("produtos")) {
+        
+        let produtos = JSON.parse(localStorage.getItem("produtos")); // JSON.parse() - transforma um texto em array
+
+        $("#tblProdutos tbody").html(""); // limpa a table
+
+        produtos.forEach((produto) => {
+            $("#tblProdutos tbody").append(`
+            <tr>
+                <td>${produto.nome}</td>
+                <td>${produto.qntd}</td>
+                <td>${produto.valor}</td>
+                <td style="width: 20px;"><button type="button" class="btn btn-info">E</button></td>
+                <td style="width: 20px;"><button type="button" class="btn btn-danger">D</button></td>
+            </tr>`) // append acrescenta uma tag ou um script no html
+        });
+        
+    }
+}
 
 $(() => {
     //Código executado no carregamento da página
-    // alert("Mensagem apresentada no carregamento do sistema");
 
     $("#btnSalvar").click(() => {
-        console.log("FUNÇÃO CLICK FUNCIONOU")
+
+        let produto = {};
+        produto.nome = $("#nmProduto").val(); // guarda o valor do id nmProduto
+        produto.qntd = $("#quantidade").val();
+        produto.valor = $("#vlrProduto").val();
+
+        let listaProdutos = [];
+
+        if (localStorage.getItem("produtos")) {
+            listaProdutos = JSON.parse(localStorage.getItem("produtos"));
+        }
+
+        listaProdutos.push(produto); // insere o produto no array ListaProduto
+
+        //localStorage.setItem("key", "efetivo valor que será armazenado"); // cookies - as informações ficam salvas até o usuário limpar o cache
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos)); //JSON.stringify() - tranforma o array em string
+
+        alert("Produto salvo com sucesso!")
+        
+        $("#nmProduto").val("") // se for usado val(""), o conteúdo do id será "apagado" (sobrescreve o conteúdo)
+        $("#quantidade").val("")
+        $("#vlrProduto").val("")
+
+        populaTabela();
     })
 })
